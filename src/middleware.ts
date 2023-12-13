334394a5f5a881a043941023cf0b9aa201a29cba
Waffen <waffen.sultanofc@gmail.com>
2023-12-13 22:17:02 +0800

    first commit

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+    const res = NextResponse.next();
+    const supabase = createMiddlewareClient({ req, res });
+
+    // Retrieve current session
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) {
+        // User is authenticated
+        return res;
+    }
+
+    // Authentication check failed so redirect user to login
+    return NextResponse.redirect(new URL("/login", req.url));
+}
+
+export const config = {
+    matcher: "/",
+};
