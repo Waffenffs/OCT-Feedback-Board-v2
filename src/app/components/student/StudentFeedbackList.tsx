32c55f51eeabcfdd99ded04356a03037bf2560b3
Waffen <waffen.sultanofc@gmail.com>
2023-12-27 13:34:31 +0800

    Updated FilterMenu.tsx and data fetching

--- src/app/components/student/StudentFeedbackList.tsx
+++ src/app/components/student/StudentFeedbackList.tsx
@@ -6,52 +6,110 @@ import { useState, useEffect } from "react";
 import StudentFeedbackCard from "./StudentFeedbackCard";
 import FilterMenu from "../ui/FilterMenu";
 
-export type TFeedbackStatus = "Pending" | "Resolved" | "Flagged";
+export type TFeedbackStatus = "Pending" | "Resolved" | "Flagged" | "All";
 export type TSort = "Newest to Oldest" | "Oldest to Newest" | "Alphabetical";
 
 export default function StudentFeedbackList() {
     const [feedbacks, setFeedbacks] = useState<any | null>(null);
     const [isActiveFilterMenu, setIsActiveFilterMenu] = useState(false);
     const [activeCategory, setActiveCategory] =
-        useState<TFeedbackStatus>("Pending");
+        useState<TFeedbackStatus>("All");
     const [activeSort, setActiveSort] = useState<TSort>("Newest to Oldest");
     const [isLoading, setIsLoading] = useState(true);
 
     const supabase = createClientComponentClient();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                // Fetch user's email
-                const {
-                    data: { user },
-                    error: userError,
-                } = await supabase.auth.getUser();
+    const fetchData = async () => {
+        try {
+            // Fetch user's email
+            const {
+                data: { user },
+                error: userError,
+            } = await supabase.auth.getUser();
 
-                if (userError) throw userError;
+            if (userError) throw userError;
 
-                const userUID = user?.id;
+            const userUID = user?.id;
 
-                // Fetch user's feedbacks
-                const { data: feedbacksData, error: feedbacksError } =
-                    await supabase
-                        .from("feedbacks")
-                        .select("*")
-                        .eq("feedback_creator_uid", userUID);
+            // Fetch user's feedbacks
+            const { data: feedbacksData, error: feedbacksError } =
+                await supabase
+                    .from("feedbacks")
+                    .select("*")
+                    .eq("feedback_creator_uid", userUID);
 
-                if (feedbacksError) throw feedbacksError;
+            if (feedbacksError) throw feedbacksError;
 
-                setFeedbacks(feedbacksData);
-                setIsLoading(false);
-            } catch (error) {
-                console.error(
-                    `Error fetching data for feedback list: ${error}`
+            let filteredFeedbacks = feedbacksData || [];
+
+            if (activeCategory) {
+                filteredFeedbacks = filterByStatus(
+                    feedbacksData,
+                    activeCategory
                 );
             }
-        };
 
+            if (activeSort) {
+                filteredFeedbacks = sortByDate(filteredFeedbacks, activeSort);
+            }
+
+            setFeedbacks(filteredFeedbacks);
+            setIsLoading(false);
+        } catch (error) {
+            console.error(`Error fetching data for feedback list: ${error}`);
+        }
+    };
+
+    useEffect(() => {
         fetchData();
     }, []);
+
+    const filterByStatus = (
+        feedbacks: any[],
+        status: TFeedbackStatus
+    ): any[] => {
+        if (status === "All") {
+            return feedbacks;
+        }
+
+        return feedbacks.filter(
+            (feedback) => feedback.feedback_status === status
+        );
+    };
+
+    const sortByDate = (feedbacks: any[], sort: TSort): any[] => {
+        const sorted = feedbacks.slice();
+
+        switch (sort) {
+            case "Newest to Oldest":
+                sorted.sort(
+                    (a, b) =>
+                        new Date(b.feedback_created_at).getTime() -
+                        new Date(a.feedback_created_at).getTime()
+                );
+                break;
+            case "Oldest to Newest":
+                sorted.sort(
+                    (a, b) =>
+                        new Date(a.feedback_created_at).getTime() -
+                        new Date(b.feedback_created_at).getTime()
+                );
+                break;
+            case "Alphabetical":
+                sorted.sort((a, b) =>
+                    a.feedback_title.localeCompare(b.feedback_title)
+                );
+        }
+
+        return sorted;
+    };
+
+    const handleApplyFilters = () => {
+        setIsLoading(true);
+        setIsActiveFilterMenu(false);
+
+        fetchData();
+    };
 
     if (isLoading)
         return (
@@ -105,10 +163,12 @@ export default function StudentFeedbackList() {
 
                     {isActiveFilterMenu && (
                         <FilterMenu
+                            setIsActiveFilterMenu={setIsActiveFilterMenu}
                             activeCategory={activeCategory}
                             activeSort={activeSort}
                             setActiveCategory={setActiveCategory}
                             setActiveSort={setActiveSort}
+                            handleApplyFilters={handleApplyFilters}
                         />
                     )}
                 </div>
--- src/app/components/ui/FilterMenu.tsx
+++ src/app/components/ui/FilterMenu.tsx
@@ -1,45 +1,60 @@
+import { useDetectClickOutside } from "react-detect-click-outside";
+
 import type { TFeedbackStatus, TSort } from "../student/StudentFeedbackList";
 
 type TFilterMenuProps = {
+    setIsActiveFilterMenu: React.Dispatch<React.SetStateAction<boolean>>;
     activeCategory: TFeedbackStatus;
     setActiveCategory: React.Dispatch<React.SetStateAction<TFeedbackStatus>>;
     activeSort: TSort;
     setActiveSort: React.Dispatch<React.SetStateAction<TSort>>;
+    handleApplyFilters(): void;
 };
 
 export default function FilterMenu({
+    setIsActiveFilterMenu,
     activeCategory,
     setActiveCategory,
     activeSort,
     setActiveSort,
+    handleApplyFilters,
 }: TFilterMenuProps) {
     const activeStyles =
         "border-blue-400 text-blue-500 bg-neutral-100 font-semibold";
     const inactiveStyles = "text-slate-600 bg-neutral-200";
 
+    const ref = useDetectClickOutside({
+        onTriggered: () => setIsActiveFilterMenu(false),
+    });
+
     return (
-        <article className='z-50 absolute -translate-x-80 mt-1 bg-[#1c1c1c] rounded shadow-xl px-5 py-3'>
+        <article
+            ref={ref}
+            className='z-50 absolute -translate-x-[26rem] mt-1 bg-[#1c1c1c] rounded shadow-xl px-5 py-3'
+        >
             <section>
                 <h2 className='text-lg text-green-500 font-bold'>Categories</h2>
                 <ul className='flex flex-row justify-start gap-3 mt-2 rounded'>
-                    {["Pending", "Resolved", "Flagged"].map((el, index) => {
-                        const currentStylings =
-                            el === activeCategory
-                                ? activeStyles
-                                : inactiveStyles;
+                    {["All", "Pending", "Resolved", "Flagged"].map(
+                        (el, index) => {
+                            const currentStylings =
+                                el === activeCategory
+                                    ? activeStyles
+                                    : inactiveStyles;
 
-                        return (
-                            <ul
-                                key={index}
-                                onClick={() =>
-                                    setActiveCategory(el as TFeedbackStatus)
-                                }
-                                className={`${currentStylings} text-xs transition duration-300 cursor-pointer w-24 py-1 px-3 rounded-full border-2 text-sm text-center`}
-                            >
-                                <span>{el}</span>
-                            </ul>
-                        );
-                    })}
+                            return (
+                                <ul
+                                    key={index}
+                                    onClick={() =>
+                                        setActiveCategory(el as TFeedbackStatus)
+                                    }
+                                    className={`${currentStylings} text-xs transition duration-100 cursor-pointer w-24 py-1 px-3 rounded-full border-2 text-sm text-center`}
+                                >
+                                    <span>{el}</span>
+                                </ul>
+                            );
+                        }
+                    )}
                 </ul>
             </section>
 
@@ -59,7 +74,7 @@ export default function FilterMenu({
                                 htmlFor={`sort-${index}`}
                                 key={index}
                                 onClick={() => setActiveSort(el as TSort)}
-                                className={`${currentStylings} transition duration-300 cursor-pointer border-2 px-3 py-2 text-xs w-full flex flex-row justify-between items-center rounded`}
+                                className={`${currentStylings} transition duration-100 cursor-pointer border-2 px-3 py-2 text-xs w-full flex flex-row justify-between items-center rounded`}
                             >
                                 <span>{el}</span>
 
@@ -74,6 +89,18 @@ export default function FilterMenu({
                         );
                     })}
                 </ul>
+
+                <footer className='w-full mt-6 flex flex-row gap-3 justify-end items-center tracking-wider font-semibold text-xs'>
+                    <button onClick={() => setIsActiveFilterMenu(false)}>
+                        Cancel
+                    </button>
+                    <button
+                        onClick={() => handleApplyFilters()}
+                        className='py-2 px-4 rounded bg-green-500 border border-green-500 hover:bg-green-600 hover:border-green-400 hover:text-slate-200 transition duration-200 ease-in-out'
+                    >
+                        Apply
+                    </button>
+                </footer>
             </section>
         </article>
     );
