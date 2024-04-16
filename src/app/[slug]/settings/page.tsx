c9a2fe99aa6710787edbe0b6fa2e989d22b4b6c7
Waffen Sultan <waffen.sultanofc@gmail.com>
2024-04-16 20:27:55 +0800

    Rewrote and cleaned up code

--- src/app/[slug]/settings/page.tsx
+++ src/app/[slug]/settings/page.tsx
@@ -42,40 +42,20 @@ export default function Settings() {
         fetchSettingsData();
     }, []);
 
-    const settingComponents: Record<TCurrentSetting, React.JSX.Element> = {
-        Password: <PasswordSettings />,
-        Account: <></>,
-        FAQ: <></>,
-    };
-
     return (
         <div className='w-full h-full flex flex-col justify-center items-center'>
             <main className='w-4/6'>
                 <header className='w-full bg-gradient-to-r from-green-500 via-lime-500 to-lime-500 rounded-full shadow py-4 px-7'>
                     <h1 className='text-5xl font-bold tracking-tight text-white'>
-                        Settings |{" "}
+                        Settings
+                        {/* Settings |{" "}
                         <span className='text-3xl'>
                             {data?.account?.account_name}
-                        </span>
+                        </span> */}
                     </h1>
                 </header>
 
-                <div className='flex flex-row justify-start items-center gap-2 mt-5'>
-                    {["Password", "FAQ", "Account"].map((val, index) => (
-                        <div
-                            key={index}
-                            className={`${
-                                val === currentSetting
-                                    ? "bg-blue-500 text-white hover:bg-blue-600 border border-blue-400"
-                                    : "bg-white text-black hover:bg-blue-500 hover:text-white"
-                            } cursor-pointer transition duration-200 shadow flex justify-center items-center text-center font-semibold  w-20 py-1 px-7 rounded text-sm semibold rounded-xl`}
-                        >
-                            {val}
-                        </div>
-                    ))}
-                </div>
-
-                {settingComponents[currentSetting]}
+                <PasswordSettings accountName={data?.account?.account_name} />
             </main>
         </div>
     );
